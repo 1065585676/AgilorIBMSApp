63da3b7765b0227624354dfaf1572bf9f3131620
1065585676 <1065585676@qq.com>
2017-06-29 13:43:44 +0800

    new data insert file

--- dabases/insertData.sql
+++ dabases/insertData.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS ibmsappdb;
 
 USE ibmsappdb;
 
+DROP TABLE IF EXISTS point;
 CREATE TABLE IF NOT EXISTS point (
 
 	point_id INT NOT NULL AUTO_INCREMENT,
@@ -17,3 +18,9 @@ CREATE TABLE IF NOT EXISTS point (
 INSERT INTO point ( point_name, point_url, point_type, point_value_type ) VALUES ('LIG_SCENE_706','http://localhost/AgilorReader/devices/BIAD_LIGHT_SCENE_DEVICES/LIG_SCENE_706','SCENE','ENUM');
 INSERT INTO point ( point_name, point_url, point_type, point_value_type ) VALUES ('LIG_SCENE_703','http://localhost/AgilorReader/devices/BIAD_LIGHT_SCENE_DEVICES/LIG_SCENE_703','SCENE','ENUM');
 
+INSERT INTO point ( point_name, point_url, point_type, point_value_type ) VALUES ('LIG_OTHER_708_SHANG_ZHAO_DENG_S','http://localhost/AgilorReader/devices/BIAD_LIGHT_DEVICES/LIG_OTHER_708_SHANG_ZHAO_DENG_S','CONTROL','LONG');
+INSERT INTO point ( point_name, point_url, point_type, point_value_type ) VALUES ('LIG_OTHER_708_SHANG_ZHAO_DENG_M','http://localhost/AgilorReader/devices/BIAD_LIGHT_DEVICES/LIG_OTHER_708_SHANG_ZHAO_DENG_M','CONTROL','LONG');
+INSERT INTO point ( point_name, point_url, point_type, point_value_type ) VALUES ('LIG_OTHER_708_SHANG_ZHAO_DENG_N','http://localhost/AgilorReader/devices/BIAD_LIGHT_DEVICES/LIG_OTHER_708_SHANG_ZHAO_DENG_N','CONTROL','LONG');
+
+INSERT INTO point ( point_name, point_url, point_type, point_value_type ) VALUES ('LIG_708_21_S','http://localhost/AgilorReader/devices/BIAD_LIGHT_DEVICES/LIG_708_21_S','SINGLE','LONG');
+INSERT INTO point ( point_name, point_url, point_type, point_value_type ) VALUES ('LIG_708_21_N','http://localhost/AgilorReader/devices/BIAD_LIGHT_DEVICES/LIG_708_21_N','SINGLE','LONG');
